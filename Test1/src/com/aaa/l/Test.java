55da7b346b2971f3db9a6aa6627b16e36f00424d
1375942531 <1375942531@qq.com>
2018-05-31 13:27:12 +0800

    new 一个对象

--- Test1/src/com/aaa/l/Test.java
+++ Test1/src/com/aaa/l/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("ldl²Ù×÷");
 		System.out.println(111);
+		String a = new String("12331");
+		System.out.println(a);
 	}
 
 }
